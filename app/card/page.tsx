f5d6d1eaf13f604572c5d6adc7fd92a115baebb2
rendraa07 <rendraakma.rnd.111@gmail.com>
2026-05-06 22:06:20 +0800

    quiz

---
+++ app/card/page.tsx
@@ -0,0 +1,49 @@
+export default function learnpage() {
+    return (
+        <div className="bg-white h-screen flex text-blue-950 gap-4">
+        <div className="bg-red-300 h-72 w-96 p-8 flex flex-col justify-between rounded-2xl">
+            <div className="flex gap-4">
+                <img src={"/palette.svg"} alt="icon palette" />
+                <p>web Design</p>
+            </div>
+            <div>
+                <h1 className="text-4xl font-bold">one page</h1>
+                <p>explore</p>
+            </div>
+        </div>
+
+        <div className="bg-red-300 h-72 w-96 p-8 flex flex-col justify-between rounded-2xl">
+            <div className="flex gap-4">
+                <img src={"/palette.svg"} alt="icon palette" />
+                <p>web Design</p>
+            </div>
+            <div>
+                <h1 className="text-4xl font-bold">one page</h1>
+                <p>explore</p>
+            </div>
+        </div>
+
+        <div className="bg-red-300 h-72 w-96 p-8 flex flex-col justify-between rounded-2xl">
+            <div className="flex gap-4">
+                <img src={"/palette.svg"} alt="icon palette" />
+                <p>web Design</p>
+            </div>
+            <div>
+                <h1 className="text-4xl font-bold">one page</h1>
+                <p>explore</p>
+            </div>
+        </div>
+
+        <div className="bg-red-950 h-72 w-96 p-8 flex flex-col justify-between rounded-2xl">
+            <div className="flex gap-4">
+                <img src={"/palette.svg"} alt="icon palette" />
+                <p>web Design</p>
+            </div>
+            <div>
+                <h1 className="text-4xl font-bold">one page</h1>
+                <p>explore</p>
+            </div>
+        </div>
+        </div>
+    );
+}
